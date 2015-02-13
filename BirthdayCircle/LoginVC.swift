dedbdd3785bf6e88534ce1a3fa8274d320f56349
fjswhl <340403800@qq.com>
2015-02-13 21:05:03 +0800

    commit

--- BirthdayCircle/LoginVC.swift
+++ BirthdayCircle/LoginVC.swift
@@ -93,6 +93,7 @@ class LoginVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
                 return
             }
         }
+        println(cell.frame)
         return cell
     }
     
